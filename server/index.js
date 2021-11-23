3856341aa6fddd7740ba9b993d9f91884fba6f59
Ziek <zieksanchez3@gmail.com>
2021-11-23 13:17:25 -0600

    finally can run mysql from local network and run server from wsl

--- server/index.js
+++ server/index.js
@@ -3,20 +3,20 @@ const app = express();
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "username",
+  host: "192.168.96.1",
+  user: "newUser",
   password: "password",
   database: "node",
 });
 
-// db.connect(function (err) {
-//   if (err) throw err;
-//   console.log("You are now connected...");
-// });
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("You are now connected...");
+});
 
 app.get("/", (req, res) => {
   const sqlInsert =
-    "INSERT INTO tabletable (name, username, password) VALUES ('Ziek', 'bruhdot777', '50519')";
+    "INSERT INTO tabletable (name, username, password) VALUES ('Tyler', 'superTy', 'coochiecowboy')";
   db.query(sqlInsert, (err, results) => {
     res.send("Hello World!");
   });
@@ -25,3 +25,11 @@ app.get("/", (req, res) => {
 app.listen(3301, () => {
   console.log("running on port 3301");
 });
+
+//Troubles with connecting wsl to local network server (this changes!!!)
+//had to retrieve local machine ip address and use that in code
+//also had to create a new user in mysql adding one line at a time in Xammp
+//works now!
+
+
+//cat /etc/resolv.conf
