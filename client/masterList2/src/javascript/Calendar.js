32115f9a46a4fb574326d841dea8952e14df09ce
Ziek <eas260@txstate.edu>
2022-01-04 08:13:31 -0600

    calendar display works properly. Need to add functionality

--- client/masterList2/src/javascript/Calendar.js
+++ client/masterList2/src/javascript/Calendar.js
@@ -6,10 +6,11 @@ import "../css/index.css";
 
 class Calendar extends React.Component {
   static contextType = DateContext;
+
   constructor(props) {
     super(props);
     this.state = {
-        dateObject: new Date(),
+      dateObject: new Date()
     };
   }
 
@@ -17,104 +18,123 @@ class Calendar extends React.Component {
     //let contextData = this.context; //grab context data with this line
     const date = new Date();
     this.setState({
-        dateObject: date,
+      dateObject: date
     });
   }
 
-  getMonthName() {
+  getMonthAndYear() {
     const monthNames = ["January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"
     ];
 
-    return monthNames[this.state.dateObject.getMonth()];
+    return monthNames[this.state.dateObject.getMonth()] + " " + this.state.dateObject.getFullYear();
   }
 
-  fillCalendar() {
+  fillCalendar(current) {
     let newRows = [],
-      currentDate = this.state.dateObject,
-      lastOfPrevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() , 0 ).getDate(),
+      today = new Date(),
+      currentDate = current,
+      lastOfPrevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate(),
       lastOfCurrentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate(),
-      weekDayOfFirst = new Date(currentDate.getFullYear(), currentDate.getMonth()  , 1 );
+      weekDayOfFirst = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
 
     for (let i = 0; i < weekDayOfFirst.getDay() - 1; i++) {
-      newRows[i] = (<p className="calendarGrid__point calendarGrid__point-notCurrent" key={i}>{lastOfPrevMonth -= 1}</p>);
+      newRows[i] = (
+        <p className="calendarGrid__point calendarGrid__point-notCurrent" key={i}>{lastOfPrevMonth -= 1}</p>);
     }
     newRows = newRows.reverse();
 
     for (let i = 0; i <= lastOfCurrentMonth - 1; i++) {
       let newNode = null;
 
-      if (i === currentDate.getDate()) {
-        newNode = (<p className="calendarGrid__point calendarGrid__point--currentDay" key={i + 55}>{i + 1}</p>)
-      }
-      else {
-        newNode = (<p className="calendarGrid__point" key={i + 55}>{i + 1}</p>)
+      if (i === today.getDate() - 1 && currentDate.getMonth() === today.getMonth() && currentDate.getFullYear() === today.getFullYear()) {
+        newNode = (<p className="calendarGrid__point calendarGrid__point--currentDay" key={i + 55}>{i + 1}</p>);
+      } else {
+        newNode = (<p className="calendarGrid__point" key={i + 55}>{i + 1}</p>);
       }
       newRows.push(newNode);
     }
 
     for (let i = 0; newRows.length < 35; i++) {
-      let newNode = (<p className="calendarGrid__point calendarGrid__point-notCurrent" key={i + 110}>{i + 1}</p>)
+      let newNode = (<p className="calendarGrid__point calendarGrid__point-notCurrent" key={i + 110}>{i + 1}</p>);
       newRows.push(newNode);
     }
 
     return newRows;
   }
 
+  previousMonth = () => {
+    let newDate = this.state.dateObject;
+    newDate.setMonth(newDate.getMonth() - 1);
+    this.setState({
+      dateObject: newDate
+    });
+  };
+
+  nextMonth = () => {
+    let newDate = this.state.dateObject;
+    newDate.setMonth(newDate.getMonth() + 1);
+    this.setState({
+      dateObject: newDate
+    });
+  };
+
   render() {
     return (
       <div id="calendar">
         <div id="calendarHeader">
           <p id="calendarHeader__date" className="calendarHeader--background">
-            {this.getMonthName()}
+            {this.getMonthAndYear()}
           </p>
-          <img className="calendarHeader__arrow" src={calendarArrowUp} alt={"arrowUp"}/>
-          <img className="calendarHeader__arrow" src={calendarArrowDown} alt={"arrowDown"}/>
+          <div id="calendarHeader__arrow--flex">
+            <img className="calendarHeader__arrow" src={calendarArrowUp} onClick={this.previousMonth} alt={"arrowUp"} />
+            <img className="calendarHeader__arrow" src={calendarArrowDown} onClick={this.nextMonth} alt={"arrowDown"} />
+          </div>
         </div>
         <div id="calendarGrid">
-            <p
-              className="calendarGrid__point calendarGrid__point--weekHeading"
-              id="monday"
-            >
-              M
-            </p>
-            <p
-              className="calendarGrid__point calendarGrid__point--weekHeading"
-              id="monday"
-            >
-              T
-            </p>
-            <p
-              className="calendarGrid__point calendarGrid__point--weekHeading"
-              id="monday"
-            >
-              W
-            </p>
-            <p
-              className="calendarGrid__point calendarGrid__point--weekHeading"
-              id="monday"
-            >
-              T
-            </p>
-            <p
-              className="calendarGrid__point calendarGrid__point--weekHeading"
-              id="monday"
-            >
-              F
-            </p>
-            <p
-              className="calendarGrid__point calendarGrid__point--weekHeading"
-              id="monday"
-            >
-              S
-            </p>
-            <p
-              className="calendarGrid__point calendarGrid__point--weekHeading"
-              id="monday"
-            >
-              S
-            </p>
-          {this.fillCalendar()}
+          <p
+            className="calendarGrid__point calendarGrid__point--weekHeading"
+            id="monday"
+          >
+            M
+          </p>
+          <p
+            className="calendarGrid__point calendarGrid__point--weekHeading"
+            id="monday"
+          >
+            T
+          </p>
+          <p
+            className="calendarGrid__point calendarGrid__point--weekHeading"
+            id="monday"
+          >
+            W
+          </p>
+          <p
+            className="calendarGrid__point calendarGrid__point--weekHeading"
+            id="monday"
+          >
+            T
+          </p>
+          <p
+            className="calendarGrid__point calendarGrid__point--weekHeading"
+            id="monday"
+          >
+            F
+          </p>
+          <p
+            className="calendarGrid__point calendarGrid__point--weekHeading"
+            id="monday"
+          >
+            S
+          </p>
+          <p
+            className="calendarGrid__point calendarGrid__point--weekHeading"
+            id="monday"
+          >
+            S
+          </p>
+          {this.fillCalendar(this.state.dateObject)}
         </div>
       </div>
     );
